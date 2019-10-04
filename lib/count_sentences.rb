2df77c034f98df94a0de236a1d1114b0567a401d
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-04 02:17:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,7 +6,7 @@ class String
     if self.end_with?(" . ")
       return true
     else 
-      false
+     return false
     end
   end
 
@@ -14,7 +14,7 @@ class String
     if self.end_with?(" ? ")
       return true
     else
-       false
+       return false
     end
   end
 
@@ -22,7 +22,7 @@ class String
     if self.end_with?(" ! ")
       return true
     else
-       false
+       return false
     end
   end
 
